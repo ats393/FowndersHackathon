2b6c386391ee0cce476e2bff11ede9e807b10d37
Anthony <ats393@nyu.edu>
2016-11-04 19:44:50 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+app.set('view engine', 'hbs');
 
-
+app.get('/', function(req,res){
+  res.render('home', {});
+});
 
 
 app.listen(3000);
