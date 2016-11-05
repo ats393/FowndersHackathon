e58638fc5d728b839474b1816efe787a0acf7d71
Anthony <ats393@nyu.edu>
2016-11-04 20:33:12 -0400

    twitter connected

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var publicPath = path.resolve(__dirname, "public");
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 app.use(express.static(publicPath));
 app.set('view engine', 'hbs');
 
+client.stream('statuses/filter', {track: 'election'},  function(stream) {
+  stream.on('data', function(tweet) {
+    console.log(tweet.text);
+  });
+
+
 app.get('/', function(req,res){
   res.render('home', {});
+});
+app.post('/', function(req,res){
+  search = req.body.search;
+
 });
 
 
